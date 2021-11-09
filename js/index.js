cade0e6d68134cfe6e54f6f5151d145650090a24
Nash Boisvert <fenderfan@live.ca>
2021-11-08 19:05:24 -0700

    progress on nav bar

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 (function () {
-    window.addEventListener('load', ()=>{
-        
+    window.addEventListener('load', ()=> {
+        ToggleNav()
     })
+    
+    function ToggleNav () {
+        const toggle = document.querySelector('.menu-icon')
+        toggle.addEventListener('click', () => {
+            document.querySelector('nav').classList.toggle('active')
+        })
+    }
 })()
