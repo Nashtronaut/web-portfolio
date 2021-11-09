808704aba33738187d4f9cd2e54293d45959486d
Nash Boisvert <fenderfan@live.ca>
2021-11-08 23:56:11 -0700

    skills page populates from skills.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 (function () {
     window.addEventListener('load', ()=> {
         ToggleNav()
+        PopulateSkills()
     })
     
     function ToggleNav () {
@@ -8,5 +9,42 @@
         toggle.addEventListener('click', () => {
             document.querySelector('nav').classList.toggle('active')
         })
+    }
+    
+    function PopulateSkills () {
+        let injectionTemplate = ``
+        let prof = ''
+        
+        skills.sort((a, b) => b.progress - a.progress)
+        
+        skills.forEach((skill) =>{
+            if (skill.progress === 100){
+                prof = 'Mastered'
+            }else if (skill.progress >= 80){
+                prof = 'Skilled'
+            }else if (skill.progress > 70){
+                prof = 'Comfortable'
+            }else if (skill.progress > 60){
+                prof = 'Still Learning'
+            }else{
+                prof = 'Willing/Want to Learn'
+            }
+            
+            
+            let template = `<div class="skill">
+      <img src="assests/png/${skill.img}" alt="${skill.skill} logo">
+      <h2>${skill.skill}</h2>
+      <p>Proficiency: ${prof}</p>
+      <div class="progress-container">
+        <div class="progress-bar" style="width: ${skill.progress}%"></div>
+      </div>
+      <p>${skill.progress}%</p>
+    </div> `
+            
+            injectionTemplate += template
+        })
+        
+        const insertionDiv = document.querySelector('.skill-flex-container')
+        insertionDiv.insertAdjacentHTML('afterbegin', injectionTemplate)
     }
 })()
